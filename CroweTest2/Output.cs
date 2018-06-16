ab3a48a686781824e56df690fe96b622d7661b5f
marko pavlovic <markopavlovic922@outlook.com>
2018-06-15 21:09:39 -0500

    Fixing missing files

---
+++ CroweTest2/Output.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    abstract class OutputStrategy
+    {
+
+        public abstract string Output(string msg);
+
+    }
+
+    class ConsoleOutput : OutputStrategy
+    {
+
+        public override string Output(string msg)
+        {
+
+            Console.WriteLine(msg);
+
+            return "ConsoleOutput";
+
+        }
+
+    }
+
+
+
+    class DatabaseOutput : OutputStrategy
+    {
+
+        public override string Output(string msg)
+        {
+
+            //can use ConfigurationManager.AppSettings["DBConnectionString"] in the future
+
+            Console.WriteLine("Write " + msg + " to DB here...");
+
+            return "DatabaseOutput";
+
+        }
+
+    }
+
+
+
+    class FileOutput : OutputStrategy
+    {
+
+        public override string Output(string msg)
+        {
+
+            //can use ConfigurationManager.AppSettings["FilePath"] in the future
+
+            Console.WriteLine("Write " + msg + " to File here...");
+
+            return "FileOutput";
+
+        }
+
+    }
+
+    //Would use strategy pattern and act as the API
+
+    class MessageOutput
+    {
+
+        private string _msg;
+
+        private OutputStrategy _outputstrategy;
+
+        public void SetOutputStrategy(OutputStrategy outputstrategy)
+        {
+
+            this._outputstrategy = outputstrategy;
+
+        }
+
+        public void SetMessage(string msg)
+        {
+
+            _msg = msg;
+
+        }
+
+        public string Output()
+        {
+
+            return _outputstrategy.Output(_msg);
+
+        }
+
+    }
+
+}
+
---
+++ CroweTest2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Output;
+
+namespace CroweTest
+{
+    class Program
+    {
+
+        static void Main()
+        {
+
+            MessageOutput myMessage = new MessageOutput();
+            myMessage.SetMessage(ConfigurationSettings.AppSettings["Message"]);
+
+            switch (ConfigurationSettings.AppSettings["Strategy"])
+            {
+
+                case "Database":
+
+                    myMessage.SetOutputStrategy(new DatabaseOutput());
+                    break;
+
+                case "File":
+
+                    myMessage.SetOutputStrategy(new FileOutput());
+
+                    break;
+
+                case "Console":
+
+                default:
+
+                    myMessage.SetOutputStrategy(new ConsoleOutput());
+
+                    break;
+
+            }
+
+            string result = myMessage.Output();          
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+/*       
+
+//app.config
+
+<?xml version="1.0"?>
+
+<configuration>
+
+  <startup>
+
+    <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.0"/>
+
+  </startup>
+
+  <appSettings>
+
+    <add key="Message" value="Hello World" />
+
+    <add key="Strategy" value="Console" />
+
+  </appSettings>
+
+</configuration>
+
+*/
+
+
